b4e693c0dddcaa653aeb20c7be8c2bb71ec6bded
arkhostenebris <111609606+arkhostenebris@users.noreply.github.com>
2022-08-20 01:24:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello world!");
+console.log("The website is manually coded by Arkhos Tenebris!");
+
+var a = 50;
+var b = 101;
+var c = b-a;
+console.log(c);
+
+var n = 2;
+var c = n*2;
+
+while (c<=1024) {
+	console.log(c);
+	c *= 2;
+}
